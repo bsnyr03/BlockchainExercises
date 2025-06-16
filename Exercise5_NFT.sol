42fb63b03fc3c884da9a7ace15819cd15d0792d1
bsnyr03 <barisenyer@icloud.com>
2025-06-16 23:43:43 +0200

    updates

---
+++ Exercise5_NFT.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestNFT is ERC721URIStorage, Ownable {
+    constructor() ERC721("Test NFT", "TNF") Ownable(msg.sender){}
+
+    function mintToken(address to, uint256 tokenId, string memory uri) external onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+}

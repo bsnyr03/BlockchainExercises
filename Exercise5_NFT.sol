a3eba0235c9143f1e3a3d386f1b65a044139986c
bsnyr03 <barisenyer@icloud.com>
2025-06-17 15:16:07 +0200

    test

--- Exercise5_NFT.sol
+++ Exercise5_NFT.sol
@@ -7,9 +7,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract TestNFT is ERC721URIStorage, Ownable {
     constructor() ERC721("Test NFT", "TNF") Ownable(msg.sender){}
 
-    function mintToken(address to, uint256 tokenId, string memory uri) external onlyOwner {
+    function mintToken(address to, uint256 tokenId) external onlyOwner {
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
     }
 
+    function setTokenURI(uint256 tokenId, string memory uri) external onlyOwner {
+        _setTokenURI(tokenId, uri);
+    }
 }
